7b2f1d364f4bdbaeeb8ad60fdb14fd4f2233ce71
nuhptr <nugrahaadi733@gmail.com>
2023-09-25 19:19:59 +0700

    layout: add (root) layout

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  )
+}
