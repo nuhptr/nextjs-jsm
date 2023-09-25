562bd14fbd50fc024c3c68d92ce44b4bb3e21e78
nuhptr <nugrahaadi733@gmail.com>
2023-09-25 20:36:18 +0700

    comp: formatting and update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Footer() {
-  return <div>Footer</div>
+  return (
+    <footer className='text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-16 py-12 max-md:flex-col'>
+      <p className='max-md:text-center'>
+        Copyright &copy; 2023 TS Mastery Pro | All Right Reserved
+      </p>
+
+      <div className='flex gap-x-9'>
+        <Link href='/terms-of-use'>Terms & Conditions</Link>
+        <Link href='/privacy-policy'>Privacy Policy</Link>
+      </div>
+    </footer>
+  )
 }
