a36c296e37b74bde58b4ee52a693c1ac663b0120
nuhptr <nugrahaadi733@gmail.com>
2023-09-26 11:51:24 +0700

    sanity: generated file

---
+++ sanity/lib/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from 'next-sanity'
+
+import { apiVersion, dataset, projectId, useCdn } from '../env'
+
+export const client = createClient({ apiVersion, dataset, projectId, useCdn })
