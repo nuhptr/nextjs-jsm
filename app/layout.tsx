008e87226163a6e9723b97f5e471686bd80f3e35
nuhptr <nugrahaadi733@gmail.com>
2023-09-28 14:54:40 +0700

    update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,15 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'TSM V2 | Learn Management System',
   description: 'JSM V2 is a Learn Management System for JSM students',
+  other: {
+    'theme-color': '#0d1117',
+    'color-scheme': 'dark only',
+    'twitter:card': 'summary_large_image',
+    'twitter:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    'og:url': 'https://nextjs-tsm.vercel.app/',
+    'og:image': 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+    'og:type': 'website',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
